c643606f5202c01be0262b22f12a432786af2505
Mohammad sobuj <mohammad@sobuj.gov>
2020-09-14 07:03:11 +0600

    hello add

---
+++ hello.js
@@ -0,0 +1,10 @@
+console.log('hello well come github tutorial')
+arr=[1,2,3,4,5,6,7,8,9]
+arr.forEach(function(value){
+    console.log(value)
+});
+
+var result=arr.filter(function(value){
+    return value%3==0
+})
+console.log(result)
